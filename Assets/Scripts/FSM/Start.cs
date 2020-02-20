cf6fc8e8742bb4aa160dd21bdf49be8dc8dcb297
jackchen1210 <jack3492843@gmail.com>
2020-02-20 23:24:24 +0800

    FSM update

--- Assets/Scripts/FSM/Start.cs
+++ Assets/Scripts/FSM/Start.cs
@@ -10,4 +10,6 @@ public class Start :State
         Debug.Log(battleBehaviour.BattleLog);
         yield break;
     }
+
+    
 }
---
+++ Assets/SimpleFSM.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFSM : MonoBehaviour
+{
+    [SerializeField]private State state=new State();
+    private enum State{
+
+        start,
+        running,
+        pause,
+        end,
+    }
+
+    private void Start() {
+        StartCoroutine(GameStart());
+    }
+
+    private IEnumerator GameStart(){
+        Debug.Log("GameStart");
+        yield return new WaitForSeconds(1);
+        if(state==State.running){
+            StartCoroutine(GameRunning());
+        }else{
+            StartCoroutine(GameStart());
+        }
+        
+    }
+    private IEnumerator GameRunning(){
+        Debug.Log("GameRunning");
+        yield return null;
+        StartCoroutine(GameRunning());
+    }
+    private IEnumerator GamePause(){
+        Debug.Log("GamePause");
+        yield return null;
+    }
+    private IEnumerator GameEnd(){
+        Debug.Log("GameEnd");
+        yield return null;
+    }
+
+}
